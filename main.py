4e9b9a7f8fb169b51b00c37f52f61af327f40d55
elifbabaoglu2 <62393418+elifbabaoglu2@users.noreply.github.com>
2020-03-19 21:02:31 -0400

    Writing the second function
    Here I added my second function. This function works for strings that have different lengths. Since the question didn't specify whether the length of the strings, I created this function in case the input strings have different lengths. In this function I am checking whether each unique character in s1 can be mapped into a character in s2 and making sure that same characters are mapped into the same character in s2. For instance, this function would yield True with inputs s1: fooo s2:bo.

--- main.py
+++ main.py
@@ -8,3 +8,14 @@ def has_one_one_mapping(s1, s2):
                  return False
          return True
      return False
+def len_different_one_one_mapping(s1, s2):
+     set1 = set()
+     set2 = set()
+     for c in s1:
+         set1.add(c)
+     for c in s2:
+         set2.add(c)
+     if len(set1) >= len(set2):
+         return True
+     else:
+         return False
